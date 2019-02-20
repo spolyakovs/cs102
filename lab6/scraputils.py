efd3d845dc045749f7e7951a4dfe3d6d897414ef
Unknown <sergpolyakow@mail.ru>
2019-02-20 15:14:17 +0300

    Fixed scraputils for news without author
    And added waiting time while parse multible pages

--- lab6/scraputils.py
+++ lab6/scraputils.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 def extract_news(parser):
@@ -8,24 +9,27 @@ def extract_news(parser):
     table = parser.table.find_all("table", {"class": "itemlist"})[0]
     headers = table.find_all("a", {"class": "storylink"})
     infos = table.find_all("td", {"class": "subtext"})
-    scores = table.find_all("span", {"class": "score"})
-    authors = table.find_all("a", {"class": "hnuser"})
     for i in range(len(headers)):
         title = headers[i].text
         url = headers[i]["href"]
-        comments = infos[i].find_all("a")[3].text.split("\xa0")[0]
-        try:
-            comments = int(comments)
-        except ValueError:
+        info_a = infos[i].find_all("a")
+        if len(info_a) < 4:
             comments = 0
-        author = authors[i].text
-        points = scores[i].text.split(" ")[0]
+            author = ""
+            points = 0
+        else:
+            points = int(infos[i].find_all("span", {"class": "score"})[0].text.split(" ")[0])
+            author = infos[i].find_all("a", {"class": "hnuser"})[0].text
+            if info_a[3].text == "discuss":
+                comments = 0
+            else:
+                comments = int(info_a[3].text.split("\xa0")[0])
         row = {
             "author": author,
-            "comments": comments,
-            "points": points,
             "title": title,
-            "url": url
+            "url": url,
+            "comments": comments,
+            "points": points
         }
         news_list.append(row)
     return news_list
@@ -40,7 +44,9 @@ def get_news(url, n_pages=1):
     """ Collect news from a given web page """
     news = []
     while n_pages:
+        tmin = 30
         print("Collecting data from page: {}".format(url))
+        start_time = time.clock()
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         news_list = extract_news(soup)
@@ -48,12 +54,11 @@ def get_news(url, n_pages=1):
         url = "https://news.ycombinator.com/" + next_page
         news.extend(news_list)
         n_pages -= 1
+        finish_time = time.clock()
+        print("Waiting ", tmin - finish_time + start_time, " sec")
+        time.sleep(tmin - finish_time + start_time)
     return news
 
 
 if __name__ == "__main__":
-    url = "https://news.ycombinator.com/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    for i in extract_news(soup):
-        print(str(i) + "\r\n")
+    get_news("https://news.ycombinator.com/")
